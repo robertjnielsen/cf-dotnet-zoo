1abfc6b16407fff9d77bae185f081b7971d7b726
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-25 22:24:58 -0700

    Owl class created.

---
+++ Zoo/Zoo/Classes/Owl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    public class Owl : Bird
+    {
+        /// <summary>
+        /// Determines that Owl objects are Nocturnal.
+        /// </summary>
+        public override bool Nocturnal { get => base.Nocturnal; set => base.Nocturnal = true; }
+
+        /// <summary>
+        /// Determines that Own objects are not Social.
+        /// </summary>
+        public override bool Social { get => base.Social; set => base.Social = false; }
+
+        /// <summary>
+        /// Constructor method to instantiate a new Owl object.
+        /// </summary>
+        /// <param name="name">The name of the new Owl object.</param>
+        /// <param name="lifeSpan">The lifespan of the new Owl object.</param>
+        public Owl(string name, int lifeSpan)
+        {
+            this.Name = name;
+            this.Lifespan = lifeSpan;
+        }
+
+        /// <summary>
+        /// Tells the Owl object to fly.
+        /// </summary>
+        /// <returns>Returns a string about the Owl object flying.</returns>
+        public override string Fly()
+        {
+            return $"{this.Name} flies through the night sky...";
+        }
+
+        /// <summary>
+        /// Tells the Owl object to eat.
+        /// </summary>
+        /// <returns>Returns a string about the Owl object eating.</returns>
+        public override string Eat()
+        {
+            return $"{base.Eat()} some field mice!";
+        }
+    }
+}
