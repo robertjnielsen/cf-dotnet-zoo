92e4e1fa0ace590331c2051916a88157fecd134b
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-25 22:39:57 -0700

    Mammal class created.

---
+++ Zoo/Zoo/Classes/Mammal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    public abstract class Mammal : Animal
+    {
+        /// <summary>
+        /// Determines whether the Mammal object is domesticated or not.
+        /// </summary>
+        public virtual bool IsDomesticated { get; set; } = false;
+
+        /// <summary>
+        /// Tells the Mammal object to pet you.
+        /// </summary>
+        /// <returns>Returns a string about the Mammal object petting you.</returns>
+        public virtual string PetHuman()
+        {
+            return $"{this.Name} pets you...";
+        }
+    }
+}
