511aabba820dd8c4aaad6c006aa9e7c99c022969
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-25 22:17:27 -0700

    Bird class created.

---
+++ Zoo/Zoo/Classes/Bird.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Classes
+{
+    public abstract class Bird : Animal
+    {
+        /// <summary>
+        /// Determines if the Bird is social or not.
+        /// </summary>
+        public virtual bool Social { get; set; } = true;
+
+        /// <summary>
+        /// Tells the Bird object to fly.
+        /// </summary>
+        /// <returns>Returns a string about the Bird object flying.</returns>
+        public abstract string Fly();
+    }
+}
