2cee06d3ab7f9a02a37eb31018fdae72ac864d6b
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-03-29 15:28:50 -0700

    ISleep and IPoop interfaces created.

---
+++ Zoo/Zoo/Interfaces/IPoop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo.Interfaces
+{
+    interface IPoop
+    {
+        /// <summary>
+        /// Tells the object to poop.
+        /// </summary>
+        /// <returns>Returns a string about the object pooping.</returns>
+        public string Poop();
+    }
+}
